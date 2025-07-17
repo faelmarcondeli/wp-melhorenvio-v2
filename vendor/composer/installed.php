5b23d9a340f2ed110be35ed8acbe15f37c2f8a01
faelmarcondeli <64538814+faelmarcondeli@users.noreply.github.com>
2025-07-17 14:22:03 -0300

    Add vendor autoload for plugin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'melhorenvio/melhorenvio-cotation',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6b426862abd12539b40ab276a8bee9a5a9ac9afd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'melhorenvio/melhorenvio-cotation' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6b426862abd12539b40ab276a8bee9a5a9ac9afd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
